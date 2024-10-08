02d1a16f28673aaa0f1fbe9474262a13aaf4e60c
anil <naishadhamanil@g~mail.com>
2024-10-08 13:32:31 +0530

    added3

--- variables.sh
+++ variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Ramesh: Hello suresh, gm"
-echo "Suresh: Hello Ramesh, Vgm"
+echo "$PERSON1: Hello $PERSON2, gm"
+echo "$PERSON2: Hello $PERSON1, Vgm"
 echo "this is new learning"
